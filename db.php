7c0a1268c79fcee8ad15a29dcf5b7ca9d2b2cbdd
Guillaume Marcel <55285167+marcelguillaume@users.noreply.github.com>
2024-12-15 23:58:41 +0100

    Add deploy file

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+function init_database()
+{
+    $host = $_ENV['DB_HOST'];
+    $port = $_ENV['DB_PORT'];
+    $dbname = $_ENV['DB_NAME'];
+    $user = $_ENV['DB_USER'];
+    $password = $_ENV['DB_PASSWORD'];
+
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname";
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+}
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Load dependencies
+require 'vendor/autoload.php';
+
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Load database
+require 'db.php';
+
+try {
+    $pdo = init_database();
+} catch (PDOException $e) {
+    echo 'Attention ! Erreur de connexion à la base de données : ' . $e->getMessage();
+}
+
+
+echo 'Bravo ! Vous avez réussi a déployer votre application !';
